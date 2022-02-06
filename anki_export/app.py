2fc62456490d20c62253a4ec340ba900738fe984
Pacharapol Withayasakpunt <polv@polv.cc>
2022-02-06 21:46:03 +0700

    add docstring

--- anki_export/app.py
+++ anki_export/app.py
@@ -1,4 +1,5 @@
 import sqlite3
+from typing import Any
 from zipfile import ZipFile
 from tempfile import mkdtemp
 import shutil
@@ -8,21 +9,50 @@ from pathlib import Path
 
 
 class ApkgReader:
-    def __init__(self, apkg_path: str):
-        self.temp_dir = mkdtemp()
-        with ZipFile(apkg_path) as zf:
-            zf.extractall(self.temp_dir)
+    """Main class for reading *.apkg files."""
 
-        db_path = Path(self.temp_dir).joinpath("collection.anki21")
-        if not db_path.exists():
-            db_path = db_path.with_name("collection.anki2")
+    def __init__(
+        self,
+        apkg_path: str | Path,
+        sqlite_path: str | Path = None,
+        extract_to: str | Path = None,
+    ):
+        """Main class for reading *.apkg files.
 
-        self.conn = sqlite3.connect(str(db_path))
+        Args:
+            apkg_path (str | Path): Path to the *.apkg file.
+            sqlite_path (str | Path, optional): Path to the actual SQLite file. Defaults to None.
+            extract_to (str, optional): Path to the extraction folder. Defaults to `mkdtemp()`.
+        """
+
+        self.tempdir = False
+        if extract_to:
+            Path(extract_to).mkdir(parents=True, exist_ok=True)
+            self.extract_to = str(extract_to)
+        else:
+            self.extract_to = mkdtemp()
+            self.tempdir = True
+
+        with ZipFile(str(apkg_path)) as zf:
+            zf.extractall(self.extract_to)
+
+        self.sqlite_path = Path(self.extract_to).joinpath("collection.anki21")
+        if not self.sqlite_path.exists():
+            self.sqlite_path = self.sqlite_path.with_name("collection.anki2")
+
+        if sqlite_path:
+            self.sqlite_path.rename(sqlite_path)
+
+        self.conn = sqlite3.connect(str(self.sqlite_path))
         self.conn.row_factory = sqlite3.Row
 
         self.models = None
         self.decks = None
-        self.init()
+
+        cursor = self.conn.execute("SELECT * FROM col")
+        col = next(cursor)
+        self.models: dict[str, Any] = json.loads(col["models"])
+        self.decks: dict[str, Any] = json.loads(col["decks"])
 
     def __enter__(self):
         return self
@@ -37,27 +67,37 @@ class ApkgReader:
         return self.notes
 
     def close(self):
-        self.conn.close()
-        shutil.rmtree(self.temp_dir)
+        """Close the database connection and clean up"""
 
-    def init(self):
-        cursor = self.conn.execute("SELECT * FROM col")
-        col = next(cursor)
-        self.models = json.loads(col["models"])
-        self.decks = json.loads(col["decks"])
+        self.conn.close()
+
+        if self.tempdir:
+            shutil.rmtree(self.extract_to)
 
     def find_model_by_id(self, mid):
+        """Find model by ID"""
+
         return self.models.get(str(mid), None)
 
     def find_deck_by_id(self, did):
+        """Find deck by ID"""
+
         return self.decks.get(str(did), None)
 
     def find_note_by_id(self, nid):
+        """Find note by ID"""
+
         cursor = self.conn.execute("SELECT * FROM notes WHERE id=?", (nid,))
         return self._format_note(next(cursor))
 
     @property
     def notes(self):
+        """Generate an iterator of notes
+
+        Yields:
+            OrderedDict: dictionary of notes
+        """
+
         for note in self.conn.execute("SELECT * FROM notes"):
             yield self._format_note(note)
 
@@ -75,11 +115,19 @@ class ApkgReader:
         return [x["name"] for x in sorted(model["flds"], key=lambda x: x["ord"])]
 
     def find_card_by_id(self, cid):
+        """Find card by ID"""
+
         cursor = self.conn.execute("SELECT * FROM cards WHERE id=?", (cid,))
         return self._format_card(next(cursor))
 
     @property
     def cards(self):
+        """Generate an iterator of cards
+
+        Yields:
+            OrderedDict: dictionary of cards
+        """
+
         for card in self.conn.execute("SELECT * FROM cards"):
             yield self._format_card(card)
 
@@ -97,12 +145,32 @@ class ApkgReader:
         return card
 
     def cards_by_ord(self, ord=0):
+        """Get card by ord (position in Note Type)
+
+        Args:
+            ord (int, optional): position in Note Type. Defaults to 0.
+
+        Yields:
+            OrderedDict: dictionary of cards
+        """
+
         for formatted_card in self.cards:
             if formatted_card["ord"] == ord:
                 yield formatted_card
 
-    def export(self, has_header=True, has_deck=True, ord=0):
-        result = OrderedDict()
+    def export(self, has_header=True, has_deck=True, ords: list[int] = [0]):
+        """Export for pyexcel_xlsxwx (OrderedDict of 2-D Lists)
+
+        Args:
+            has_header (bool, optional): Whether to add header on the first row. Defaults to True.
+            has_deck (bool, optional): Whether to add deck names to the output Defaults to True.
+            ords (list[int], optional): positions in Note Type. Defaults to [0].
+
+        Returns:
+            OrderedDict: a suitable format for pyexcel_xlsxwx
+        """
+
+        result: dict[str, list[list[str]]] = OrderedDict()
 
         if has_header:
             for model in self.models.values():
@@ -115,30 +183,25 @@ class ApkgReader:
 
                 result[model["name"]] = [header]
 
-        for formatted_card in self.cards_by_ord(ord=ord):
-            record = list()
-            record.extend(formatted_card["data"]["note"]["flds"].split("\x1f"))
-            record.append(formatted_card["data"]["note"]["tags"])
+        for ord in ords:
+            for formatted_card in self.cards_by_ord(ord=ord):
+                record = list()
+                record.extend(formatted_card["data"]["note"]["flds"].split("\x1f"))
+                record.append(formatted_card["data"]["note"]["tags"])
 
-            if has_deck:
-                record.append(self.find_deck_by_id(formatted_card["did"])["name"])
+                if has_deck:
+                    record.append(self.find_deck_by_id(formatted_card["did"])["name"])
 
-            result[
-                self.find_model_by_id(formatted_card["data"]["note"]["mid"])["name"]
-            ].append(record)
+                result[
+                    self.find_model_by_id(formatted_card["data"]["note"]["mid"])["name"]
+                ].append(record)
+
+        to_be_del = []
+        for k, v in result.items():
+            if len(v) == 1:
+                to_be_del.append(k)
+
+        for k in to_be_del:
+            del result[k]
 
         return result
-
-
-if __name__ == "__main__":
-    import pyexcel_xlsxwx
-    from time import time
-
-    start = time()
-
-    with ApkgReader(
-        "/Users/patarapolw/PycharmProjects/AnkiTools/tests/input/test.apkg"
-    ) as apkg:
-        pyexcel_xlsxwx.save_data("test.xlsx", apkg.export(), config={"format": None})
-
-    print(time() - start)
